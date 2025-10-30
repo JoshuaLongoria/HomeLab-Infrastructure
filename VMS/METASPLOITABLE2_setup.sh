f1f584697fdabb92a1b0416cb2f8a99c3ecbbf4e
JoshuaLongoria <49418998+JoshuaLongoria@users.noreply.github.com>
2025-10-30 10:59:47 -0500

    Update METASPLOITABLE2_setup.sh with documentation
    Added documentation for METASPLOITABLE2 setup including purpose, default credentials, and known vulnerabilities.

--- VMS/METASPLOITABLE2_setup.sh
+++ VMS/METASPLOITABLE2_setup.sh
@@ -1,2 +1,21 @@
+
 Since this is a pre-built vulnerable VM, you'll mainly store documentation here, 
 such as default usernames/passwords and reminders of its known vulnerabilities.
+
+
+| Machines                                   | CPU                         | RAM |  STORGE |
+|-----------------------------------------------|----------------------------|---------|-------|
+| METASPLOITABLE2     | 1 | 512mb   |8GB|
+
+#Purpose
+Intentionally vulnerable Linux VM for penetration testing practice
+
+#Default Username
+msfadmin
+
+#Default Password
+msfadmin
+
+#Known Vulnerabilities
+FTP, SSH, Telnet, Samba, VNC, PostgreSQL, MySQL 
+default credentials/weak configurations, and common web application flaws like SQL injection and command injection.
